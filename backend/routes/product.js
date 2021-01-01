e7b35e95c199677aae3576962380f5c5e86767b6
Jay Bhushan <jaybhushan011@gmail.com>
2021-01-01 16:12:17 +0530

    priceTotal

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,13 +1,21 @@
 const router = require('express').Router();
 
 let Product = require('../models/product.model');
-
+let price = 0;
 
 router.route('/').get((req, res) => {
   Product.find()
   .then(products => res.json(products))
   .catch(err => res.status(400).json('Error : ' + err));
 });
+
+router.route('/priceCalculate').post( (req, res) => {
+  price = req.body.total;
+})
+router.route('/priceCalculate').get( (req, res) => {
+  res.send(price)
+  price = 0;
+})
 
 router.route('/getBooks').get((req, res) => {
   Product.find( {Category : "Books" })
