1bffa17fa6429ae935bc1dcdc13a72bc034c9e64
Saravana Chilamakuri <saravanac5a@gmail.com>
2021-01-01 16:08:54 +0530

    Total

--- src/components/cartcomponent.js
+++ src/components/cartcomponent.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import './cartcomponent.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-
+var toadd=0
 export default function CartComp(props){
     console.log(props.id)
     const [finalinfo,setfinalinfo]=useState([])
+    const [priceofproduct,setpriceofproduct]=useState(0)
+
+    function total(qty,price){
+        console.log(qty,price)
+        toadd=toadd+qty*price
+        setpriceofproduct(toadd/6)
+    }    
 
     async function getArray(id){
         if(finalinfo.length===0){
         const res = await Axios.post('http://localhost:5000/product/getProduct',{"id":id})
         await setfinalinfo(res.data)
+        await total(props.qty,res.data.price)
+        
         }
+        //useEffect(total(props.qty,finalinfo.price),[])
 }
+
+
 getArray(props.id)
     return(
     <div>
@@ -25,8 +37,10 @@ getArray(props.id)
                         <div className="min-30"><h5 className="width-1">{finalinfo.title}</h5></div>
                         <div className="min-10"><h5 className="width-2">{props.qty}</h5></div>
                         <div className="min-10"><h5 className="width">$ {finalinfo.price}</h5></div>
+                        <div className="min-10"><h5 className="width">$ {finalinfo.price*props.qty}</h5></div>
                         <div className="min-10"><button className="btn btn-primary">Delete</button></div>
                     </div>
+                    
                 </tbody>
     </div>
     )
