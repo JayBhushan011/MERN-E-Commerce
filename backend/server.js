1bfb1ac4f1aa72b45a9e1ad295734cb9dd7fd7e2
JayBhushan011 <70096832+JayBhushan011@users.noreply.github.com>
2021-06-10 15:39:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.get("/", function(request, response) {
   response.send("<h1>Hello World!</h1>");
 })
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function(){
   console.log(`Server is running at port ${PORT}`);
