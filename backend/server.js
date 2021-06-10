3e4d9fc296b6304940675b980f8e484e35ffaa21
JayBhushan011 <70096832+JayBhushan011@users.noreply.github.com>
2021-06-10 16:00:46 +0530

    edit for deployment

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,14 @@ connection.once('open', () => {
 })
 // *******
 
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 const userRouter = require('./routes/user');
 app.use('/user', userRouter);
